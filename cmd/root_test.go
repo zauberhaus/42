bc38703c5444de4fd74733fabca00638bd5c796e
Dirk <dirk@lembke.nz>
2022-03-28 15:20:01 +1300

    add sub command test

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -1,12 +1,32 @@
+/*
+Copyright © 2021 Dirk Lembke <dirk@lembke.nz>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package cmd_test
 
 import (
+	"bytes"
+	"fmt"
 	"os"
 	"testing"
 
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
 	"github.com/zauberhaus/42/cmd"
+	"github.com/zauberhaus/42/logger"
+	"gopkg.in/yaml.v3"
 )
 
 var (
@@ -89,4 +109,63 @@ func TestRunEnv(t *testing.T) {
 
 	os.Unsetenv("NAME")
 	os.Unsetenv("VALUE")
+}
+
+func TestRunSubCommand(t *testing.T) {
+	rootCmd := cmd.NewRootCmd(
+		&cobra.Command{Use: "root-test",
+			Short: "A tool to watch and export Kubernetes logs to sentry",
+			Run: func(cmd *cobra.Command, args []string) {
+				assert.Equal(t, t.Name(), config.Name)
+				assert.Equal(t, 7365, config.Value)
+			},
+		},
+	)
+
+	version := cmd.NewVersion("today", "123456", "v1.1.1", "dirty")
+	rootCmd.SetVersion(version)
+
+	rootCmd.WithInit(func(rc *cmd.RootCommand) {
+		rc.Flags().StringP("name", "n", "", "Name")
+		cmd.BindCmdFlag(rc.Flags(), "name")
+
+		rc.Flags().IntP("value", "v", 0, "Value")
+		cmd.BindCmdFlag(rc.Flags(), "value")
+	})
+
+	rootCmd.WithSubCommands(func(rc *cmd.RootCommand) {
+		var versionCmd = &cobra.Command{
+			Use:   "version",
+			Short: "Show the version info",
+			Run: func(cmd *cobra.Command, args []string) {
+
+				data, err := yaml.Marshal(rc.GetVersion())
+				if err != nil {
+					logger.Error("Invalid version: %v", err)
+				}
+
+				fmt.Fprintln(cmd.OutOrStderr(), string(data))
+			},
+		}
+
+		rc.AddCommand(versionCmd)
+	})
+
+	rootCmd.SetConfig(&config)
+	rootCmd.SetArgs([]string{
+		"version",
+	})
+
+	output := &bytes.Buffer{}
+	rootCmd.SetOut(output)
+	err := rootCmd.Execute()
+	if assert.NoError(t, err) {
+		var info cmd.Version
+		err := yaml.Unmarshal(output.Bytes(), &info)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, version, &info, "Invalid value")
+	}
 }
